97a39598bac41b0a8949f3096824361ac0dba3cd
emanzelekha <emanzelekha@gmail.com>
2017-09-14 11:04:30 +0200

    f

---
+++ app/src/main/java/com/example/ok/tagreb/Toast_Image.java
@@ -0,0 +1,13 @@
+package com.example.ok.tagreb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Toast_Image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toast__image);
+    }
+}
