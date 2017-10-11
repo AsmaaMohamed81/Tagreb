fdb1b4f002ca4951d9c995bf52a64fd7ebe08600
emanzelekha <emanzelekha@gmail.com>
2017-10-11 15:57:26 +0200

    nnn

--- app/src/main/java/com/example/ok/tagreb/jj.java
+++ app/src/main/java/com/example/ok/tagreb/jj.java
@@ -4,5 +4,5 @@ package com.example.ok.tagreb;
  * Created by ok on 11/10/2017.
  */
 
-public class hh {
+public class jj {
 }
--- app/src/main/java/com/example/ok/tagreb/ll.java
+++
@@ -1,8 +0,0 @@
-package com.example.ok.tagreb;
-
-/**
- * Created by ok on 08/10/2017.
- */
-
-public class ll {
-}
