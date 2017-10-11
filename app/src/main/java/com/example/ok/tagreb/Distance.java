8108cdf442b42ebebff23135e7a7a76234a630f0
emanzelekha <emanzelekha@gmail.com>
2017-10-11 13:35:19 +0200

    f

---
+++ app/src/main/java/com/example/ok/tagreb/Distance.java
@@ -0,0 +1,14 @@
+package com.example.ok.tagreb;
+
+/**
+ * Created by ok on 11/10/2017.
+ */
+public class Distance {
+    public String text;
+    public int value;
+
+    public Distance(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/com/example/ok/tagreb/Route.java
@@ -0,0 +1,24 @@
+package com.example.ok.tagreb;
+
+/**
+ * Created by ok on 11/10/2017.
+ */
+
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+/**
+ * Created by Mai Thanh Hiep on 4/3/2016.
+ */
+public class Route {
+    public Distance distance;
+    public Duration duration;
+    public String endAddress;
+    public LatLng endLocation;
+    public String startAddress;
+    public LatLng startLocation;
+
+    public List<LatLng> points;
+}
